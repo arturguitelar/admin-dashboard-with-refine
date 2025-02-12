b79877c592d097361c55dba13b1e5c2940acea5f
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-12 12:37:22 -0300

    feat: Initialize WebSocket client only in browser, using access token

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,8 +1,11 @@
 import { GraphQLClient } from '@refinedev/nestjs-query';
+import { createClient } from 'graphql-ws';
 
 import { fetchWrapper } from './fetch-wrapper';
 
+export const API_BASE_URL = 'https://api.crm.refine.dev';
 export const API_URL = 'https://api.crm.refine.dev';
+export const WS_URL = 'wss://api.crm.refine.dev/graphql';
 
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
@@ -13,3 +16,19 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+
+export const wsClient =
+  typeof window !== 'undefined'
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem('access_token');
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
