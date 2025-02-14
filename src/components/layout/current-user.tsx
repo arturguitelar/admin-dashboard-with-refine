8910f1c01aecc9d097ed82378c54de9465192c38
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-14 13:29:58 -0300

    feat: Add avatar name and accont settings button to popover

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,19 +1,62 @@
-import { Popover } from 'antd';
+import { useState } from 'react';
+import { Button, Popover } from 'antd';
 import { useGetIdentity } from '@refinedev/core';
+import { SettingOutlined } from '@ant-design/icons';
+
 import type { User } from '@/graphql/schema.types';
 
 import { CustomAvatar } from '../custom-avatar';
+import { Text } from '../text';
 
 export const CurrentUser = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { data: user } = useGetIdentity<User>();
+
+  const content = (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Text strong style={{ padding: '12px 20px' }}>
+        {user?.name}
+      </Text>
+
+      <div
+        style={{
+          borderTop: '1px solid #d9d9d9',
+          padding: '4px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '4px',
+        }}
+      >
+        <Button
+          style={{ textAlign: 'left' }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setIsOpen(true)}
+        >
+          Account Settings
+        </Button>
+      </div>
+    </div>
+  );
 
   return (
     <>
       <Popover
         placement="bottomRight"
         trigger="click"
-        style={{ padding: 0 }}
         zIndex={999}
+        content={content}
+        styles={{
+          body: {
+            padding: 0,
+          },
+        }}
       >
         <CustomAvatar
           name={user?.name}
