5c2d536936e91c2fb819e4e8955f9491d3f43480
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-15 16:59:21 -0300

    feat: Add new resources to navigation menu
    Adds new resources to the application's navigation menu.
    
    The following resources were added:
    
    - Companies
    - Tasks
    
    These resources are now accessible through the side navigation menu,
    providing users with quick access to these sections of the application.

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -1,4 +1,8 @@
-import { DashboardOutlined } from '@ant-design/icons';
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from '@ant-design/icons';
 import { IResourceItem } from '@refinedev/core';
 
 /**
@@ -18,6 +22,27 @@ export const resources: IResourceItem[] = [
     meta: {
       label: 'Dashboard',
       icon: <DashboardOutlined />,
+    },
+  },
+  {
+    name: 'companies',
+    list: '/companies',
+    show: '/companies/:id',
+    create: '/companies/new',
+    edit: '/companies/edit/:id',
+    meta: {
+      label: 'Companies',
+      icon: <ShopOutlined />,
+    },
+  },
+  {
+    name: 'tasks',
+    list: '/tasks',
+    create: '/tasks/new',
+    edit: '/tasks/edit/:id',
+    meta: {
+      label: 'Tasks',
+      icon: <ProjectOutlined />,
     },
   },
 ];
