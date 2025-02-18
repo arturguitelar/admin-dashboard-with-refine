f630a66f06682daf6e2bfbe829dbbaeee109e501
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-18 10:35:30 -0300

    feat(DealsChart): Implement initial DealsChart component
    Adds the DealsChart component, displaying a basic area chart
    using `@ant-design/plots`.  The chart is initially configured
    with empty data.

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,5 +1,45 @@
+import { Card } from 'antd';
+import { DollarOutlined } from '@ant-design/icons';
+import { Area, AreaConfig } from '@ant-design/plots';
+
+import { Text } from '../text';
+
 const DealsChart = () => {
-  return <div>Deals Chart</div>;
+  const config: AreaConfig = {
+    data: [],
+  };
+
+  return (
+    <Card
+      style={{
+        height: '100%',
+      }}
+      styles={{
+        header: {
+          padding: '8px 16px',
+        },
+        body: {
+          padding: '24px 24px 0 24px',
+        },
+      }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: '0.5rem' }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
 };
 
 export default DealsChart;
