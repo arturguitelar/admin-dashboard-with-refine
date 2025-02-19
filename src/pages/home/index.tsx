69be2539e0d4e76bf3ccf84db2cdb9133f6c4ed0
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-19 12:48:34 -0300

    feat(Home): Integrate DashboardTotalCountCard component
    Integrates the DashboardTotalCountCard component into the Home page,
    fetching and displaying data for companies, contacts, and deals.
    
    Key changes:
    
    *   Adds three DashboardTotalCountCard components to the Home page.
    *   Fetches data for each card using the useCustom hook.
    *   Passes the fetched data and isLoading state as props to the cards.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,49 @@
 import { Col, Row } from 'antd';
+import { useCustom } from '@refinedev/core';
 
-import { DealsChart, UpcomingEvents } from '@/components';
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from '@/graphql/queries';
+
+import {
+  DashboardTotalCountCard,
+  DealsChart,
+  UpcomingEvents,
+} from '@/components';
+import { DashboardTotalCountsQuery } from '@/graphql/types';
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  });
+
   return (
     <div>
       <Row gutter={[32, 32]}>
         <Col xs={24} sm={24} xl={8}>
-          DashboarTotalCountCard
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
         </Col>
 
         <Col xs={24} sm={24} xl={8}>
-          DashboarTotalCountCard
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
         </Col>
 
         <Col xs={24} sm={24} xl={8}>
-          DashboarTotalCountCard
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
         </Col>
       </Row>
 
