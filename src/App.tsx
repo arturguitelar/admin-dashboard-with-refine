795bd6612f909dbe086b00d4ad4ab49b459b5979
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-25 10:35:32 -0300

    feat: Add route and initial page for CompanyList
    Adds a new route "/companies" to the application's routing configuration,
    along with a corresponding CompanyList page component. This route is
    protected by authentication. The CompanyList page currently only contains
    placeholder content and will be further developed in future commits.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { BrowserRouter, Outlet, Route, Routes } from 'react-router';
 
 import { authProvider, dataProvider, liveProvider } from './providers';
 
-import { Home, Login, ForgotPassword, Register } from './pages';
+import { Home, Login, ForgotPassword, Register, CompanyList } from './pages';
 import { Layout } from './components/layout';
 import { resources } from './config/resources';
 
@@ -57,6 +57,7 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+                  <Route path="/companies" element={<CompanyList />} />
                 </Route>
               </Routes>
               <RefineKbar />
---
+++ src/pages/companyList/index.tsx
@@ -0,0 +1,3 @@
+export const CompanyList = () => {
+  return <div>CompanyListPage</div>;
+};
