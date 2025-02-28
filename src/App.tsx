6f3128f51cb0e78935560afff2b118d17a74df30
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-28 18:25:13 -0300

    feat(Tasks): Add initial TasksList route and placeholder page
    Adds a new route "/tasks" to the application's routing configuration
    in App.tsx, along with a placeholder TasksList page component
    (pages/tasks/lists.tsx). Also exports the TasksList component from
    pages/index.tsx. This sets up the initial structure for the tasks
    Kanban.

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import {
   CompanyList,
   Create,
   Edit,
+  TasksList,
 } from './pages';
 import { Layout } from './components/layout';
 import { resources } from './config/resources';
@@ -65,10 +66,15 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+
                   <Route path="/companies">
                     <Route index element={<CompanyList />} />
                     <Route path="new" element={<Create />} />
                     <Route path="edit/:id" element={<Edit />} />
+                  </Route>
+
+                  <Route path="/tasks">
+                    <Route index element={<TasksList />} />
                   </Route>
                 </Route>
               </Routes>
---
+++ src/pages/tasks/list.tsx
@@ -0,0 +1,7 @@
+export const TasksList = () => {
+  return (
+    <div>
+      <h1>Tasks List</h1>
+    </div>
+  );
+};
