dbdd18cf391f10d789859e7b954a8ce90346293e
Artur Guitelar <artur.guitelar@gmail.com>
2025-02-28 21:18:31 -0300

    feat(Tasks): Enhance KanbanColumn component with props and functionality
    Enhances the KanbanColumn component by adding props for id, title,
    description, count, data, and onAddClick. Uses these props to
    customize the column's appearance and behavior.
    
    Key changes:
    
    * Adds props for id, title, description, count, data, and onAddClick.
    * Uses props to customize column appearance and behavior.
    * Calls onAddClick prop when the add button is clicked.
    * Passes id and data props to useDroppable hook.

--- src/components/tasks/kanban/column.tsx
+++ src/components/tasks/kanban/column.tsx
@@ -1,23 +1,32 @@
-import { useDroppable } from '@dnd-kit/core';
+import { ReactNode } from 'react';
+import { useDroppable, UseDroppableArguments } from '@dnd-kit/core';
 import { Badge, Button, Space } from 'antd';
 
 import { Text } from '@/components/text';
 import { PlusOutlined } from '@ant-design/icons';
 
-export const KanbanColumn = ({ children }: React.PropsWithChildren) => {
-  const { isOver, setNodeRef, active } = useDroppable({
-    id: '',
-    data: '',
-  });
+type Props = {
+  id: string;
+  title: string;
+  description?: ReactNode;
+  count: number;
+  data?: UseDroppableArguments['data'];
+  onAddClick?: (args: { id: string }) => void;
+};
 
-  const count = 2;
-
-  const description = 'Description';
-
-  const title = 'TITLE';
+export const KanbanColumn = ({
+  children,
+  id,
+  title,
+  description,
+  count,
+  data,
+  onAddClick,
+}: React.PropsWithChildren<Props>) => {
+  const { isOver, setNodeRef, active } = useDroppable({ id, data });
 
   const onAddClickHandler = () => {
-    //
+    onAddClick?.({ id });
   };
 
   return (
